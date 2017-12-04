d5b286bb8ab9b052bee68615fc659def18ce87e9
Kalib Reid Watson <watso196@msu.edu>
2017-12-04 14:22:06 -0500

    Changed GamesController index() to match template, and changed gamepage.ctp to index.ctp for to make CakePHP happy

--- src/Controller/GamesController.php
+++ src/Controller/GamesController.php
@@ -7,8 +7,6 @@ class GamesController extends AppController
 {
 	public function index()
     	{
-        	$this->loadComponent('Paginator');
-        	$articles = $this->Paginator->paginate($this->Articles->find());
-        	$this->set(compact('articles'));
+        	
     	}
 }
